62d2bdd70a24029e055f1dde8673a79a60b48e19
omar3laa <omaralaa4526@gmail.com>
2026-07-23 04:00:01 +0300

    Change embedding model path to finetuned model

--- task1_task2.py
+++ task1_task2.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 # ==========================================
 
 embedding_model = SentenceTransformer(
-    "sentence-transformers/all-MiniLM-L6-v2"
+    "./finetuned_model"
 )
 
 # ==========================================
